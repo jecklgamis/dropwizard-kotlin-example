a152b7248b6a2d7cccb7852c172aeaee96162f23
Gary Yao <garyjyao@gmail.com>
2020-05-17 01:09:07 +1000

    add buildInfo resource

--- src/main/kotlin/dropwizard/kotlin/example/ExampleApp.kt
+++ src/main/kotlin/dropwizard/kotlin/example/ExampleApp.kt
@@ -1,7 +1,9 @@
 package dropwizard.kotlin.example
 
+import com.google.common.io.Resources
 import dropwizard.kotlin.example.filter.DiagnosticContextFilter
 import dropwizard.kotlin.example.healthcheck.DefaultHealthCheck
+import dropwizard.kotlin.example.resource.BuildInfoResource
 import dropwizard.kotlin.example.resource.RootResource
 import io.dropwizard.Application
 import io.dropwizard.setup.Environment
@@ -13,6 +15,9 @@ class ExampleApp : Application<ExampleAppConfig>() {
         env.jersey().register(RootResource(config.appName))
         env.jersey().register(DiagnosticContextFilter())
         env.healthChecks().register("default", DefaultHealthCheck())
+
+        val resource = BuildInfoResource(Resources.toString(Resources.getResource("buildInfo.json"), Charsets.UTF_8))
+        env.jersey().register(resource)
     }
 
     companion object {
